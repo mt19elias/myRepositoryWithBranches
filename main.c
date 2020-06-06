02a566d2b320942b0ea70c91a3d97655e844c672
Elias <64968139+mt19elias@users.noreply.github.com>
2020-06-06 09:43:56 +0200

    calculation in main

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "add.h"
+
 int mian(){
+double x = 1;
+double y = 2;
+
 
     printf("thsis is my calculator\n");
-
+    printf("%lf + %lf = %lf\n", x,y,myAddFcn(x,y));
 }
